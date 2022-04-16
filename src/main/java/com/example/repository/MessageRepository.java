895de1900f1caac557d9dcbad83ebdd95f21a772
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-16 08:32:42 +0300

    add log in logic and fix filter

---
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String text);
+}
