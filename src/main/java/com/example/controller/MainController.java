b7487082fe43bdc52b75c6d2e71ddb3ba0d21f04
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-16 09:25:45 +0300

    add logic if user exists and save him into db

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.controller;
 
 import com.example.entity.Message;
 import com.example.repository.MessageRepository;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepository messageRepository;
 
